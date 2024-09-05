60347f23f0d2360c0c1d800fdcb6d1df1682aaa1
Alireza Shams <alerzs1023@gmail.com>
2024-09-05 12:00:48 +0330

    trans

--- transacion/views.py
+++ transacion/views.py
@@ -1,11 +1,25 @@
 from rest_framework import generics
 from .models import Transaction
 from .serializers import TransactionSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class TransactionListCreateView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
 
-class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    def perform_create(self, serializer):
+        return serializer.save(user = self.request.user)
+    
+
+class TransactionDetailView(generics.UpdateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        return serializer.save(user = self.request.user)
+    
+
+
+
