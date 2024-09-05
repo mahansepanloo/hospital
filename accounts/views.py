38f4bae0fd665460dbbecce39b404ce5b4fc9c81
mahan <sepanloooo@gmail.com>
2024-09-05 12:14:54 +0330

    128

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics,status
 from .models import Walet
 from .serializers import WaletSerializer,UpateSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 
 class WaletListCreateView(generics.ListCreateAPIView):
     queryset = Walet.objects.all()
@@ -17,6 +19,32 @@ class WaletUpdate(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return Walet.objects.filter(user = self.request.user)
+
+class WaletListCreateView(generics.ListCreateAPIView):
+    queryset = Walet.objects.all()
+    serializer_class = WaletSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Walet.objects.filter(user=self.request.user)
+
+class WaletRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Walet.objects.all()
+    serializer_class = UpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Walet.objects.filter(user=self.request.user)
+
+    def add_amount(self, request, *args, **kwargs):
+        walet = self.get_object()
+        amount = request.data.get('amount', 0)
+
+        try:
+            walet.add_amount(amount)
+            return Response({"message": "Amount added successfully.", "new_balance": walet.wallet}, status=status.HTTP_200_OK)
+        except ValueError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
