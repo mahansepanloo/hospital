aadde16adf6653f02acd58caadd4d363e7cbe196
Alireza Shams <alerzs1023@gmail.com>
2024-09-05 11:41:05 +0330

    trans

--- transacion/models.py
+++ transacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    title = models.CharField(max_length=25)
+    date = models.DateField()
+    amount = models.FloatField()
+
+
