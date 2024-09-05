59f87b631aea99bd12122169af0cb41478486c0c
mahan <sepanloooo@gmail.com>
2024-09-05 11:39:09 +0330

    5

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Walet(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    wallet = models.IntegerField(null=True,blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
