9467449f0cca7d3a32565d08c45765a447d356f0
mahan <sepanloooo@gmail.com>
2024-09-06 12:28:11 +0330

    mahan855565454\lfgkh;lkdfmhzdlfkhnmxdgkh

--- accounts/url.py
+++ accounts/url.py
@@ -1,7 +1,7 @@
 from django.urls import path  
-from .views import WaletListCreateView,WaletUpdate
+from .views import *
 
 urlpatterns = [  
-    path('wallets/', WaletListCreateView.as_view(), name='walet-list-create'),  
-    path('wallets/<int:pk>/',WaletUpdate.as_view(), name='walet-detail'),
+    path('wallets/', Login.as_view(), name='walet-list-create'),
+    path('wallets/<int:pk>/',Refresh.as_view(), name='walet-detail'),
 ]
