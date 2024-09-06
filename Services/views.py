df68684bc78ec4e5333cc00c6dab091d2faa1fb7
Fatememdn <mohammadianfateme86@gmail.com>
2024-09-06 11:57:42 +0330

    3

--- Services/views.py
+++ Services/views.py
@@ -1,8 +1,24 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .models import Service
 from .serializers import ServiceSerializer
+from .permission import IsNurse
 
 class ServiceListAPIView(ListAPIView):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [IsNurse]
+
+class ServiceDetailAPIView(RetrieveAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsNurse]
+
+class ServiceCreateAPIView(CreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsNurse]
+
+
+
+
 
