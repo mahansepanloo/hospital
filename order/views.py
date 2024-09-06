3ff58ecb08d33063262d04eded7c1929fe7fb09a
mahan <sepanloooo@gmail.com>
2024-09-06 11:58:19 +0330

    mahan86

--- order/views.py
+++ order/views.py
@@ -1,8 +1,10 @@
-
 from rest_framework import generics
 from .models import Orders
 from .serializers import OrdersSerializer
+from .permissions import IsSuperUser
 
-class OrdersViewSet(generics.ListCreateAPIView):
+class OrdersViewSet(generics.ListAPIView):
     queryset = Orders.objects.all()
     serializer_class = OrdersSerializer
+    permission_classes = [IsSuperUser]
+
