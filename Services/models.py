761678e0761ab682a5340aeb85e807cfbee2fa75
mahan <sepanloooo@gmail.com>
2024-09-06 11:43:22 +0330

    new85

--- Services/models.py
+++ Services/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from accounts.models import Bimar
 
+class Cat(models.Model):
+    name = models.CharField(max_length=5000)
 
 class Service(models.Model):
+    category = models.ForeignKey(Cat, on_delete= models.CASCADE)
     type = models.CharField(max_length=250)
     bimar = models.ForeignKey('Bimar', on_delete= models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
     price = models.IntegerField()
+
+
 
